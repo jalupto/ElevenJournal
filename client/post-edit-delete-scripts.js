89e5fc9e9f47734fd3f9420476f0ece567326b34
Jared Upton <jalupto@gmail.com>
2021-06-14 18:13:48 -0400

    client modules continue

--- client/post-edit-delete-scripts.js
+++ client/post-edit-delete-scripts.js
@@ -2,7 +2,37 @@
  *** POST JOURNAL ***
  ************************** */
 function postJournal() { 
-    console.log('postJournal Function Called')
+    // console.log('postJournal Function Called')
+
+    const accessToken = localStorage.getItem('SessionToken')
+    let title = document.getElementById('title').value;
+    let date = document.getElementById('date').value;
+    let entry = document.getElementById('entry').value;
+
+    let newEntry = {
+        journal: {
+            title: title,
+            date: date,
+            entry: entry
+        }
+    }
+
+    fetch(`http://localhost:3000/journal/create`, {
+            method: "POST",
+            headers: new Headers({
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${accessToken}`
+            }),
+            body: JSON.stringify(newEntry)
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            displayMine()
+        })
+        .catch(err => {
+            console.error(err)
+        })
 }
 
 
